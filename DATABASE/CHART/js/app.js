00c9da8a558cdf1b5d1fe1c732ed9e5038dc1a19
zmtheus <matheus.nsantos@bandtec.com.br>
2022-05-09 01:40:57 -0300

    chart

---
+++ DATABASE/CHART/js/app.js
@@ -0,0 +1,26 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+
+    type: 'line',
+    data: {
+        labels: ['January', 'February', 'March'],
+        
+        datasets: [{
+            label: 'Gráfico',
+            backgroundColor: ['green', 'blue', 'yellow'],
+            borderColor: 'rgb(255, 99, 132)',
+            data: [50, 10, 5]
+        }]
+    },
+
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+}); 
