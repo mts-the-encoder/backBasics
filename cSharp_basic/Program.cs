2726b3dc3ea177f8311656746de759ef686dfde8
zmtheus <matheus.nsantos@bandtec.com.br>
2022-06-13 09:36:02 -0300

    expressões

--- cSharp_basic/Program.cs
+++ cSharp_basic/Program.cs
@@ -7,3 +7,11 @@ Console.Write("Digite seu ano de nascimento: ");
 int year = int.Parse(Console.ReadLine());   
 int age = 2022 - year;
 Console.WriteLine($"Sua idade é {age}");
+
+    if (age >= 18) {
+        Console.WriteLine("Você é maior de idade");
+    } else if (age.Equals(12)) {
+        Console.WriteLine("Você tem 12 anos");
+    } else {
+        Console.WriteLine("Você ainda é menor de idade");
+    }
