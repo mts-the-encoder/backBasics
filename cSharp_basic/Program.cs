20018214c81003edb1aca8dad288963d61b34d85
zmtheus <matheus.nsantos@bandtec.com.br>
2022-06-15 10:28:01 -0300

    end

--- cSharp_basic/Program.cs
+++ cSharp_basic/Program.cs
@@ -1,13 +1,4 @@
 ﻿class Program {
     static void Main() {
-        string[] names = { "mts", "matheus" };
-
-        string name = "matheus";
-
-        Console.WriteLine(name.Length);
-        Console.WriteLine(name.EndsWith("a"));
-        Console.WriteLine(name.EndsWith("s"));
-        Console.WriteLine(name.Contains("u"));
-        Console.WriteLine(name.IndexOf("us"));
-    }
+     Console.WriteLine("Hello World")
 }
