fb96bde6de56e882838df8cb6f90dd6bb3ea8547
zmtheus <matheus.nsantos@bandtec.com.br>
2022-06-15 07:30:14 -0300

    for

--- cSharp_basic/Program.cs
+++ cSharp_basic/Program.cs
@@ -1,17 +1,9 @@
-﻿Console.Write("Digite seu nome: ");
-string name = Console.ReadLine();
+﻿class Program {
+    static void Main() {
+        string[] names = { "mts", "matheus" };
 
-Console.WriteLine($"Olá {name}");  
- 
-Console.Write("Digite seu ano de nascimento: ");
-int year = int.Parse(Console.ReadLine());   
-int age = 2022 - year;
-Console.WriteLine($"Sua idade é {age}");
-
-    if (age >= 18) {
-        Console.WriteLine("Você é maior de idade");
-    } else if (age.Equals(12)) {
-        Console.WriteLine("Você tem 12 anos");
-    } else {
-        Console.WriteLine("Você ainda é menor de idade");
+        for (int i = 0; i < 2; i++) {
+            Console.WriteLine(names[i]);
+        }
     }
+}
