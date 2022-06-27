5bca1387532f1d67d0cb7dbe30c98d85a0e6ab52
zmtheus <matheus.nsantos@bandtec.com.br>
2022-06-27 11:45:04 -0300

    conversões, tipos variáveis

---
+++ FundamentosC_Sharp/Runs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int integer = 100;
+            float real = 25.5f;
+            //real = integer;
+            //integer = (int)real;
+            //string realValue = real.ToString();
+            //integer = int.Parse(realValue);
+            integer = Convert.ToInt32(real);
+            Console.WriteLine(integer);
+            //Console.WriteLine(realValue);
+            Console.WriteLine(Convert.ToBoolean(255));
+        }
+    }
+}
+
