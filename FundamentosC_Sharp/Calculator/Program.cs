9394a2493decd6dc397b5eaf7811c3230f07f438
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-03 03:54:51 -0300

    operação de subtração

--- FundamentosC_Sharp/Calculator/Program.cs
+++ FundamentosC_Sharp/Calculator/Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Sum();
+            Subtraction();
         }
 
         static void Sum()
@@ -23,6 +23,35 @@ namespace Calculator
 
             float result = valueOne + valueTwo;
             Console.WriteLine($"O resultado da soma é: {result}");
+
+            Console.ReadKey();
+        }
+
+        static void Subtraction()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro Valor: ");
+            float valueOne = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Primeiro Valor: ");
+            float valueTwo = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float result = valueOne - valueTwo;
+            Console.WriteLine($"O resultado da subtração é {result}");
+
+            Console.ReadKey();
+        }
+
+        static void Division()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float valueOne
+
         }
     }
 }
