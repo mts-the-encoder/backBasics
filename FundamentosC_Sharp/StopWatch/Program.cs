b52e572c93855c144b5eb66afcd2f29ba0079dd7
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-03 14:50:06 -0300

    iniciando

--- FundamentosC_Sharp/StopWatch/Program.cs
+++ FundamentosC_Sharp/StopWatch/Program.cs
@@ -8,6 +8,18 @@ namespace StopWatch
         {
             Console.WriteLine("Hello World");
         }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                currentTime++;
+            }
+        }
+
     }
 }
 
