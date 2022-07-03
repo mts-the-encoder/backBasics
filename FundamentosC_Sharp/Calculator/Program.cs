e8eeca6b914e728cc667e0ca8f2fe888d63489bc
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-02 22:54:49 -0300

    Operação de Soma

--- FundamentosC_Sharp/Calculator/Program.cs
+++ FundamentosC_Sharp/Calculator/Program.cs
@@ -6,7 +6,18 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            Console.Clear();
+            
+            Console.WriteLine("Primeiro valor: ");
+            float valueOne = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segubdo valor: ");
+            float valueTwo = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float result = valueOne + valueTwo;
+            Console.WriteLine($"O resultado da soma é: {result}");
         } 
     }
 }
