755e364ae89eb2dbf45bef1e3b33d66be697e0a7
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-04 02:12:25 -0300

    Obetendo as opções

--- FundamentosC_Sharp/StopWatch/Program.cs
+++ FundamentosC_Sharp/StopWatch/Program.cs
@@ -19,8 +19,12 @@ namespace StopWatch
 
             string data = Console.ReadLine().ToLower();
             char type = char.Parse(data.Substring(data.Length -1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
+
             Console.WriteLine(data);
             Console.WriteLine(type);
+            Console.WriteLine(time);
         }
 
         static void Start(int time)
