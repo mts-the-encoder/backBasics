de9c65ee2460620da1db3d2d4144c830f4983aaa
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-05 08:54:58 -0300

    comparação de strings

---
+++ FundamentosC_Sharp/Strings/Comparacao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "test";
+            Console.WriteLine(text.CompareTo("test"));
+            Console.WriteLine(text.CompareTo("tesT"));
+            Console.WriteLine(text.Contains("tesT"));
+            Console.WriteLine(text.Contains("tEsT", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(text.Equals("test"));
+            Console.WriteLine(text.Contains("detrás de ti imbécil"));
+        }
+    }
+}
