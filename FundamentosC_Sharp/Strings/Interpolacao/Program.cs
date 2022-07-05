aafbd9a58ae127e4f2b54b40e18e6dd93a5f0121
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-05 08:18:52 -0300

    interpolação de strings

--- FundamentosC_Sharp/Strings/Interpolacao/Program.cs
+++ FundamentosC_Sharp/Strings/Interpolacao/Program.cs
@@ -8,7 +8,9 @@ namespace MyApp
         {
             var price = 10.2;
             //var text = $"O preço é: {price}";
-            var text = string.Format("O Preço é: {0}", price);
+            //var text = string.Format("O Preço é: {0}", price);
+            var text = $@"O Preço é algo
+            como {price} aproximadamente";
             Console.WriteLine(text);
         }
     }
