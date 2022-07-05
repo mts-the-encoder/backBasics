ea8974d380e2b2ef97f5fc6f87756b14e2cc1c31
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-05 09:19:31 -0300

    Indices

--- FundamentosC_Sharp/Strings/Comparacao/Program.cs
+++ FundamentosC_Sharp/Strings/Comparacao/Program.cs
@@ -6,26 +6,33 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            var text = "test";
-            Console.WriteLine(text.CompareTo("test"));
-            Console.WriteLine(text.CompareTo("tesT"));
-            Console.WriteLine(text.Contains("tesT"));
-            Console.WriteLine(text.Contains("tEsT", StringComparison.OrdinalIgnoreCase));
-            Console.WriteLine(text.Equals("test"));
-            Console.WriteLine(text.Contains("detrás de ti imbécil"));
-            Console.WriteLine("\r\n");
+            var text = "test, this is a test";
+            //Console.WriteLine(text.CompareTo("test"));
+            //Console.WriteLine(text.CompareTo("tesT"));
+            //Console.WriteLine(text.Contains("tesT"));
+            //Console.WriteLine(text.Contains("tEsT", StringComparison.OrdinalIgnoreCase));
+            //Console.WriteLine(text.Equals("test"));
+            //Console.WriteLine(text.Contains("detrás de ti imbécil"));
+            //Console.WriteLine("\r\n");
+
             //StarsWith & EndsWith
 
-            Console.WriteLine(text.StartsWith("test"));
-            Console.WriteLine(text.StartsWith(" test"));
-            Console.WriteLine(text.StartsWith("Test", StringComparison.OrdinalIgnoreCase));
-            Console.WriteLine(text.StartsWith("Test"));
+            //Console.WriteLine(text.StartsWith("test"));
+            //Console.WriteLine(text.StartsWith(" test"));
+            //Console.WriteLine(text.StartsWith("Test", StringComparison.OrdinalIgnoreCase));
+            //Console.WriteLine(text.StartsWith("Test"));
 
-            Console.WriteLine("\n");
-            Console.WriteLine(text.EndsWith("t"));
-            Console.WriteLine(text.EndsWith("St"));
-            Console.WriteLine(text.EndsWith("sT"));
-            Console.WriteLine(text.EndsWith("ST", StringComparison.OrdinalIgnoreCase));
+            //Console.WriteLine("\n");
+            //Console.WriteLine(text.EndsWith("t"));
+            //Console.WriteLine(text.EndsWith("St"));
+            //Console.WriteLine(text.EndsWith("sT"));
+            //Console.WriteLine(text.EndsWith("ST", StringComparison.OrdinalIgnoreCase));
+
+            //Index
+            Console.WriteLine(text.IndexOf(","));
+            Console.WriteLine(text.IndexOf("h"));
+
         }
+
     }
 }
