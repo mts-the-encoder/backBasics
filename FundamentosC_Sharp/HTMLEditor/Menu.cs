ae50a5935807a5edff105c4ec8376ac8a1fae5b7
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-05 12:12:47 -0300

    setup projeto

---
+++ FundamentosC_Sharp/HTMLEditor/Menu.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HtmlEditor
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+
+        }
+    }
+}
