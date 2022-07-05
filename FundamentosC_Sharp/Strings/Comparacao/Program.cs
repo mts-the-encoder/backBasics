e5f9e59df421d50095107d08e82ab927b19541c2
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-05 09:11:53 -0300

    SratsWith && EndsWith

--- FundamentosC_Sharp/Strings/Comparacao/Program.cs
+++ FundamentosC_Sharp/Strings/Comparacao/Program.cs
@@ -13,6 +13,19 @@ namespace MyApp
             Console.WriteLine(text.Contains("tEsT", StringComparison.OrdinalIgnoreCase));
             Console.WriteLine(text.Equals("test"));
             Console.WriteLine(text.Contains("detrás de ti imbécil"));
+            Console.WriteLine("\r\n");
+            //StarsWith & EndsWith
+
+            Console.WriteLine(text.StartsWith("test"));
+            Console.WriteLine(text.StartsWith(" test"));
+            Console.WriteLine(text.StartsWith("Test", StringComparison.OrdinalIgnoreCase));
+            Console.WriteLine(text.StartsWith("Test"));
+
+            Console.WriteLine("\n");
+            Console.WriteLine(text.EndsWith("t"));
+            Console.WriteLine(text.EndsWith("St"));
+            Console.WriteLine(text.EndsWith("sT"));
+            Console.WriteLine(text.EndsWith("ST", StringComparison.OrdinalIgnoreCase));
         }
     }
 }
