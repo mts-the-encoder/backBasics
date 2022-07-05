0be542f7f87bf1ee8b51a13c12f7f07635e824b5
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-04 22:33:44 -0300

    strings

---
+++ FundamentosC_Sharp/Strings/GUIDS/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var id = Guid.NewGuid();
+            id.ToString();
+
+            //id = new Guid("a2f8b7a5-503e-43e2-947e-700d2bf8502c");
+            Console.WriteLine(id.ToString().Substring(0,8));
+        }
+    }
+}
+
---
+++ FundamentosC_Sharp/Strings/Interpolacao/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var price = 10.2;
+            //var text = $"O preço é: {price}";
+            var text = string.Format("O Preço é: {0}", price);
+            Console.WriteLine(text);
+        }
+    }
+}
