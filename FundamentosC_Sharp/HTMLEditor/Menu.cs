49599e7fe627f05eca2a815bdc39fd7d65323803
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-06 11:24:01 -0300

    Menu da aplicação

--- FundamentosC_Sharp/HTMLEditor/Menu.cs
+++ FundamentosC_Sharp/HTMLEditor/Menu.cs
@@ -14,6 +14,7 @@ namespace HtmlEditor
             WriteOptions();
 
             var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
         }
 
         public static void DrawScreen()
@@ -65,6 +66,25 @@ namespace HtmlEditor
             
             Console.SetCursorPosition(3, 3);
             Console.Write("Opção: ");
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Console.WriteLine("Editor");
+                    break;
+                case 2: Console.WriteLine("View");
+                    break;
+                case 0:
+                {
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                }
+                default: Show();
+                    break;
+            }
         }
     }
 }
