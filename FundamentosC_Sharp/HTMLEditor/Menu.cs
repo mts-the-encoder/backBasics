39ebf7fea6e82338b10afbad917bcb5b23c99820
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-06 09:09:33 -0300

    desenhando a tela

--- FundamentosC_Sharp/HTMLEditor/Menu.cs
+++ FundamentosC_Sharp/HTMLEditor/Menu.cs
@@ -10,7 +10,43 @@ namespace HtmlEditor
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
 
+            DrawScreen();
+        }
 
+        public static void DrawScreen()
+        {
+            DrawLine();
+
+            DrawSpace();
+
+            DrawLine();
+        }
+
+        static void DrawLine()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+
+            Console.Write("+");
+            Console.Write("\n");
+        }
+
+        static void DrawSpace()
+        {
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                Console.Write("|");
+                Console.Write("\n");
+            }
         }
     }
 }
