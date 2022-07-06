8260520ad93973ab169a35f306c8d4a91ee4d40a
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-06 18:39:23 -0300

    Editor

--- FundamentosC_Sharp/HTMLEditor/Menu.cs
+++ FundamentosC_Sharp/HTMLEditor/Menu.cs
@@ -72,7 +72,7 @@ namespace HtmlEditor
         {
             switch (option)
             {
-                case 1: Console.WriteLine("Editor");
+                case 1: Editor.Show();
                     break;
                 case 2: Console.WriteLine("View");
                     break;
