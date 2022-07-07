ab086f7c08acc9add7d4f0231a336b66815d1a62
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-07 08:31:45 -0300

    func Math

--- FundamentosC_Sharp/Moedas/Program.cs
+++ FundamentosC_Sharp/Moedas/Program.cs
@@ -9,12 +9,14 @@ namespace Numbers
         {
             Console.Clear();
 
-            decimal value = 10.25m;
-            Console.WriteLine(value.ToString(
+            decimal value = 10543.25m;
+            //Console.WriteLine(value.ToString(
                 //P, porcentagem. F
-                "C",
-                CultureInfo.CreateSpecificCulture("pt-BR")));
-
+                //"C",
+                //CultureInfo.CreateSpecificCulture("pt-BR")));
+                //Console.WriteLine(Math.Round(value)); Arredonda
+                Console.WriteLine(Math.Ceiling(value)); //Arredonda p cima
+                Console.WriteLine(Math.Floor(value)); //Arredonda p baixo
         }
     }
 }
