7c0acc18ec06dd8a6a1844782e2ed2f978577bc2
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-07 08:55:53 -0300

    forEach

--- FundamentosC_Sharp/Arrays/Program.cs
+++ FundamentosC_Sharp/Arrays/Program.cs
@@ -12,9 +12,14 @@ namespace DotnetLists
             //var myArray = new int[5];
             int[] myArray = new int[] { 15, 21, 32, 44, 50 };
 
-            for (int i = 0; i < myArray.Length; i++)
+            //for (int i = 0; i < myArray.Length; i++)
+            //{
+                //Console.WriteLine(myArray[i]);
+            //}
+
+            foreach (var item in myArray)
             {
-                Console.WriteLine(myArray[i]);
+                Console.WriteLine(item);
             }
             
         }
