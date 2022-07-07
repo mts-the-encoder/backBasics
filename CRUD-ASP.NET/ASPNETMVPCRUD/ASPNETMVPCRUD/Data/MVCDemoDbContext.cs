f570d99995742d1a60ba4651baf583cefe6a8f64
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-07 13:24:48 -0300

    Project Setup + Db Connect

---
+++ CRUD-ASP.NET/ASPNETMVPCRUD/ASPNETMVPCRUD/Data/MVCDemoDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ASPNETMVPCRUD.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETMVPCRUD.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
