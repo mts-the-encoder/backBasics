eaa1ab16953130c39919e7be5267553ed3e10802
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-07 08:19:48 -0300

    Tipo para moedas

---
+++ FundamentosC_Sharp/Moedas/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal value = 10.25m;
+            Console.WriteLine(value);
+        }
+    }
+}
