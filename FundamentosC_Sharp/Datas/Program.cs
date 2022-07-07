3df6d8f240ab5e148abc71796ca9511e67981864
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-07 00:27:29 -0300

    formatando datas

--- FundamentosC_Sharp/Datas/Program.cs
+++ FundamentosC_Sharp/Datas/Program.cs
@@ -7,12 +7,11 @@ namespace Datas
         static void Main(string[] args)
         {
             Console.Clear();
-            //var data = DateTime.Now;
-            var data = new DateTime(2022, 7, 7, 00, 10, 20);
-            Console.WriteLine(data.ToString());
-            Console.WriteLine(data.Year);
+            var data = DateTime.Now;
 
-            Console.WriteLine(data.DayOfYear);
+            // var format = String.Format("{0:yyyy-MM-dd hh:mm:ss}", data);
+            var format = $"{data}";
+            Console.WriteLine(format);
         }
     }
 }
