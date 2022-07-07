ad9e542a4fa4dc2dfa4440fab2175230f7bf437b
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-07 08:47:45 -0300

    Arrays

---
+++ FundamentosC_Sharp/Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace DotnetLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            //var myArray = new int[5];
+            int[] myArray = new int[] { new Test() };
+
+            Console.WriteLine(myArray[0].Id);
+            Console.WriteLine(myArray[1]);
+            Console.WriteLine(myArray[2]);
+            Console.WriteLine(myArray[3]);
+            Console.WriteLine(myArray[4]);
+        }
+    }
+}
+
+struct Test
+{
+    public int Id { get; set; }
+}
