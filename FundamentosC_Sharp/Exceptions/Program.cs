f61172e9f9e49b01cd01a76a348013ef848d8ee9
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-07 11:13:17 -0300

    Try/Catch

---
+++ FundamentosC_Sharp/Exceptions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[3];
+
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.InnerException);
+                //Console.WriteLine(e.Message);
+                System.Console.WriteLine("algo deu errado");
+                throw;
+            }
+        }
+    }
+}
