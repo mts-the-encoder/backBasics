3c3c35612b2879a8615ef5de2213338bfc0cbbe7
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-07 00:32:00 -0300

    Padrões de formatação

--- FundamentosC_Sharp/Datas/Program.cs
+++ FundamentosC_Sharp/Datas/Program.cs
@@ -9,9 +9,13 @@ namespace Datas
             Console.Clear();
             var data = DateTime.Now;
 
-            // var format = String.Format("{0:yyyy-MM-dd hh:mm:ss}", data);
+            //var format = String.Format("{0:yyyy-MM-dd hh:mm:ss}", data);
+            //var formatt = String.Format("{0:t}", data);
+            //var formatt = String.Format("{0:D}", data);
+            //var formatt = String.Format("{0:f}", data);
+            var formatt = String.Format("{0:r}", data);
             var format = $"{data}";
-            Console.WriteLine(format);
+            Console.WriteLine(formatt);
         }
     }
 }
