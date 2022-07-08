90567bbbb49dce6613434e0743bc0f2a27fa8791
zmtheus <matheus.nsantos@bandtec.com.br>
2022-07-08 07:24:55 -0300

    Project Setup | Db Connection | Controller - Action Method

---
+++ CRUD-ASP.NET/ASPNETMVPCRUD/ASPNETMVPCRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETMVPCRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+    }
+}
