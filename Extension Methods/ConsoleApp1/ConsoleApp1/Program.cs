2fe0b9b73c39a71ac4555efa61bd5f7872020758
zmtheus <matheus.nsantos@bandtec.com.br>
2022-08-01 13:57:45 -0300

    métodos de extensão

---
+++ Extension Methods/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acc = new Acc();
+
+            acc.PayWithCard();
+
+            acc.Pay();
+
+            int number = 10;
+
+            Console.WriteLine(number.IsBigger((10)));
+
+            Console.WriteLine(number.MultiplyFor100());
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
---
+++ Extension Methods/ConsoleApp1/ConsoleApp1/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Utils
+    {
+        public static bool IsBigger(this int param, int secondParam)
+        {
+            return param > secondParam;
+        }
+
+        public static int MultiplyFor100(this int param)
+        {
+            return param * 100;
+        }
+
+        public static void PayWithCard(this Acc acc)
+        {
+            Console.WriteLine("pagando minha conta com cartão");
+        }
+    }
+}
