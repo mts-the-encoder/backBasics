e6834d5dc83b7e16e66c4df7c423a2a2ff833f6e
zmtheus <matheus.nsantos@bandtec.com.br>
2022-08-02 14:35:02 -0300

    teste básico de static

---
+++ StaticMethods/ConsoleApp/ConsoleApp/Mat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Mat
+    {
+        public static double pi = 3.14;
+
+
+        public static int Double(int n)
+        {
+            return n * 2;
+        }
+    }
+}
---
+++ StaticMethods/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            double vpi = Mat.pi;
+            int num = 10;
+
+            Console.WriteLine(vpi);
+            Console.WriteLine(Mat.Double(num));
+        }
+    }
+}
+
